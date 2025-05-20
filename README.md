b9bd2d986ed59cac90b14d58caa036ec12a26478
Fabien Jenne <fabien.jenne@labmaite.com>
2025-05-20 09:51:00 +0200

    adding text to Readme.md

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # JobRad-CodeChallenge
 May 2025
 
-# Usage
+## Task
+- Implement a simple solution for a chat that enables customers to send messages to customer service.
+
+## Usage
 - This application is built using **Docker**
 
 - Run `run_docker_setup.sh` within this directory to build the *ChatApp* Docker image and the *chatapp-network*
@@ -9,3 +12,23 @@ May 2025
   - this starts the *chatserver* container and makes it available inside the created docker *chatapp-network*
   - additionally, it creates or assigns a docker volume *db-data* for persisting the data
 - Run `run_client.sh` to interact with the *chatserver* using the ChatClient app
+
+## Talking points
+Imagine a situation where you need to implement a chat software for our customer service to interact with our customers.
+- Quick win solution:
+  - simple chat UI for customers and customer service
+  - backend server for handling the chat functionality and database to store messages for corresponding chats
+- State-of-the-art solution:
+  - chat accessible to customers via online app or Messenger app endpoint (e.g. Whatsapp, ...)
+  - identify recurring user via cookie or token
+  - Chatbot functionality
+    - trained with most common customer questions
+    - provides quick answers on demand for simple/common questions without need of customer service personnel
+    - available 24/7
+  - ability to provide chat input via audio
+    - automatically transcribed for customer service using AI
+  - media upload functionality
+  - message-status and live-chat functionality
+  - implementation: threaded application with possibly multiple servers and load-balancing 
+  - or even split frontend, backend, and database into scalable microservices with defined APIs
+  - integration / interaction with customer service software/case management
--- code/src/Messages.py
+++ code/src/Messages.py
@@ -10,7 +10,7 @@ class SenderType(Enum):
     CUSTOMER_SERVICE = 2
 
 
-# a class for transmitting messages between client and server
+# a data format class for transmittable messages between client and server
 class BaseMessage(BaseModel):
     chat_id: str
     msg: str
@@ -20,6 +20,7 @@ class BaseMessage(BaseModel):
     is_seen: bool
 
 
+# a helper function to create a BaseMessage object
 def compose_msg(m: str, chat_id: str, sender_type: SenderType, sender_name: str):
     m = BaseMessage(
         chat_id=chat_id,
@@ -38,54 +39,3 @@ def chat_json_to_basemessages(chat_response_json):
     for m in chat_response_json:
         base_msgs.append(BaseMessage(**m))
     return base_msgs
-
-
-class MsgOrigin(Enum):
-    ClientMsg = 1
-    CompanyMsg = 2
-
-
-class MsgType(Enum):
-    TextMsg = 1
-    AudioMsg = 2
-    VideoMsg = 3
-    PhotoMsg = 4
-
-
-# class providing basic message functionality for all child classes
-class BaseMessageOld():
-    msg_origin = None
-    msg_type = None
-    sent_date = None
-    seen_date = None
-    sent_from = None
-    content = None
-
-    def __init__(self, msg_type: MsgType):
-        self.msg_type = MsgType
-
-    def set_seen(self, timestamp:dt = None):
-        if timestamp is not None:
-            self.seen_date = timestamp
-        else:
-            self.seen_date = dt.datetime.now()
-
-
-class ClientMessage(BaseMessageOld):
-    def __init__(self, msg_origin: MsgOrigin):
-        self.msg_origin = MsgOrigin.ClientMsg
-
-
-class TextMsg(BaseMessageOld):
-    def __init__(self, content=""):
-        super().__init__(MsgType.TextMsg)
-        self.content = content
-        self.sent_date = dt.datetime.now()
-
-
-class Chat():
-    msg_history = []
-
-    def chat_from_db(self, msgs: list):
-        for msg in msgs:
-            self.msg_history.append(msg)
